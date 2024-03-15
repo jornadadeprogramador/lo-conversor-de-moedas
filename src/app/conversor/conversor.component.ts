74f4142526f446c97de0a95663aa364bf70f00a7
Jornada de Programador <contato@jornadadeprogramador.com.br>
2024-03-15 15:58:24 -0300

    Buscando as moedas na API com HttpClient

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conversor',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './conversor.component.html',
   styleUrl: './conversor.component.css'
 })
-export class ConversorComponent {
-  
+export class ConversorComponent implements OnInit {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://economia.awesomeapi.com.br/json/available/uniq')
+      .subscribe((moedas: any) => {
+        console.log(moedas);
+      });
+  }
+
   calcular() {
     alert("Usando o event binding")
   }
