8a597674d814f3c1c0353c43f6dcec23ca0a6b2d
Jornada de Programador <contato@jornadadeprogramador.com.br>
2024-04-13 10:22:21 -0300

    Navegação entre páginas com Router Outlet e Router Link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ConversorComponent } from './components/conversor/conversor.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ConversorComponent]
+  imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
-  title = 'lo-conversor-de-moedas-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ConversorComponent } from './components/conversor/conversor.component';
+import { CotacoesComponent } from './components/cotacoes/cotacoes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'conversor', pathMatch: 'full' },
+    { path: 'conversor', component: ConversorComponent },
+    { path: 'cotacoes', component: CotacoesComponent }
+];
