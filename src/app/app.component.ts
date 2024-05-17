a39a6d1b1c25cf97a108f8d76463d0b74c273a4f
Jornada de Programador <contato@jornadadeprogramador.com.br>
2024-05-17 14:10:21 -0300

    Implementado Componente Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, HeaderComponent]
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent implements OnInit {
 
